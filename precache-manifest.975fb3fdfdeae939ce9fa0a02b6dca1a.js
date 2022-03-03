3aafd39d60c867f805da5abc2cda70cf163df35a
agborenow1 <agborenow.com>
2022-03-03 03:04:27 -0500

    Updates

---
+++ precache-manifest.975fb3fdfdeae939ce9fa0a02b6dca1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff66a09eea08d6ec8f4c",
+    "url": "/woca-repo/static/css/main.32cd1e06.chunk.css"
+  },
+  {
+    "revision": "ff66a09eea08d6ec8f4c",
+    "url": "/woca-repo/static/js/main.ff66a09e.chunk.js"
+  },
+  {
+    "revision": "d4e3fdec6d1045045276",
+    "url": "/woca-repo/static/css/1.72284a0b.chunk.css"
+  },
+  {
+    "revision": "d4e3fdec6d1045045276",
+    "url": "/woca-repo/static/js/1.d4e3fdec.chunk.js"
+  },
+  {
+    "revision": "9bfe9bcb46a1de9daea6",
+    "url": "/woca-repo/static/js/runtime~main.9bfe9bcb.js"
+  },
+  {
+    "revision": "73351522d3e0ef54e7358dd30d46e3e6",
+    "url": "/woca-repo/index.html"
+  }
+];
